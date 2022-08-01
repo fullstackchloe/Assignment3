aac55d3cfadbc7da28e31ceed45c4259f6cddd6d
Dawon Jun <jun00011@algonquinlive.com>
2022-08-01 03:04:30 -0400

    Modified to uses 16-sided dice and report total even and odd rolls

--- Assignment3/src/Assignment03DawonJun.java
+++ Assignment3/src/Assignment03DawonJun.java
@@ -4,20 +4,26 @@ public class Assignment03DawonJun {
 	public static void main(String[] args) {
 		
 		int roll = 0;
-		int[] diceRolls = new int[10];
-		int totalRolls = 0;
+		int[] diceRolls = new int[16];
+		int evenTotalRolls = 0;
+		int oddTotalRolls = 0;
 		
 		// sample the rolls
 		for(int count = 0; count < 1000; count++) { 
-		    roll = (int)(Math.random() * 10) + 1; 
+		    roll = (int)(Math.random() * 16) + 1; 
 		    diceRolls[roll - 1] = diceRolls[roll - 1] + 1; // adjust 1-10 value to 0-9 for index
 		}	
 		// run a report
 		for(int index = 0; index < diceRolls.length; index++) {  // crashes
 		    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
-		    totalRolls = totalRolls + diceRolls[index];
+		    if((index + 1)%2 == 0)
+		    	evenTotalRolls += diceRolls[index];
+		    else {
+		    	oddTotalRolls += diceRolls[index];
+		    }
 		}
-		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Total even rolls were: " + evenTotalRolls );
+		System.out.println("Total odd rolls were: " + oddTotalRolls );
 		System.out.println("Program by Dawon Jun");
 
 
